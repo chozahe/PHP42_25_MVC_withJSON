5e06e4b77a55b76eed8b53f0d261401b29766392
chozahe <mysligika@yndex.ru>
2025-04-17 18:58:40 +0300

    feat: добавил renderJson через трейты и обновил ApiController

--- core/Router.php
+++ core/Router.php
@@ -51,36 +51,6 @@ class Router
         }
     }
 
-    //решил создать отдельные методы для работы с json, т.к. будет нада в моей семестровой
-/*     public function resolveJson(): void
-    {
-        $path = $this->request->getUri();
-        $method = $this->request->getMethod();
-
-        if (!isset($this->routes[$method->value]) || !isset($this->routes[$method->value][$path])) {
-            http_response_code(404);
-            echo json_encode(['error' => 'Not Found']);
-            return;
-        }
-
-        $callback = $this->routes[$method->value][$path];
-
-        if (is_array($callback)) {
-            $result = call_user_func($callback, $this->request);
-
-            //чекнул что лучше всего json encode работает с массивами, так что пусть их принимает пока
-            if (is_array($result)) {
-                header('Content-Type: application/json');
-                echo json_encode($result);
-            } else {
-                echo json_encode(['error' => 'Invalid API response']);
-            }
-            return;
-        }
-        echo json_encode(['error' => 'Invalid API route']);
-    }
-
- */
     public function renderView(string $name): void
     {
         include PROJECT_ROOT . "views/$name.php";
@@ -90,7 +60,4 @@ class Router
     {
         include PROJECT_ROOT . "web/$name";
     }
-
-    
-
 }
